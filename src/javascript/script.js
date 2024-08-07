0b45ba8af154192b0a16c8095115035dd504b64e
fergsantos28 <fernandogarcia102031@gmail.com>
2024-08-06 21:39:17 -0300

    feat: scrip.js para responsividade na versao mobile

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('#mobile_btn').on('click', function() {
         $('#mobile_menu').toggleClass('active');
-        $('#mobile_btn').find('i').toggleClass('fa-x'); // Adapte a classe conforme necessário
+        $('#mobile_btn').find('i').toggleClass('fa-x');
     });
 });
